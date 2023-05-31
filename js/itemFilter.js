355006cb7a088c6b304f00bf7758196690160c36
Charles-Caltagirone <charles.dewidehem-caltagirone@laplateforme.io>
2023-05-31 15:36:41 +0200

    update itemFilter.js

--- js/itemFilter.js
+++ js/itemFilter.js
@@ -12,29 +12,43 @@ categoryParent.forEach((element) => {
   });
 });
 
+function fetchItems(url) {
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      data.forEach((element) => {
+        console.log(element);
+        let div1 = document.createElement("div");
+        let div2 = document.createElement("div");
+        let div3 = document.createElement("div");
+        let div4 = document.createElement("div");
+        let li = document.createElement("li");
+        let itemImg = document.createElement("img");
+        let itemName = document.createElement("p");
+        let itemDesc = document.createElement("p");
+        let itemLink = document.createElement("a");
+        let itemPrice = document.createElement("p");
+
+        itemImg.className = "resultsImg";
+
+        itemImg.src = `../assets/img_item/` + element.name_image;
+
+        li.append(itemImg, element.name, element.description, element.price);
+        allItems.append(li);
+      });
+    });
+}
+
+fetchItems(`traitement_filter.php`);
+
 // * générer les enfants dans le parent correspondant
 for (let i = 0; i < categoryChild.length; i++) {
   categoryChild[i].addEventListener("click", () => {
     allItems.innerHTML = "";
     // console.log(`traitement_filter.php?subCategory=` + inputRadio[i].id);
-    fetch(`traitement_filter.php?subCategory=` + categoryChild[i].id)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        data.forEach((element) => {
-          let li = document.createElement("li");
-          let itemImg = document.createElement("img");
-
-          itemImg.className = "resultsImg";
-
-          itemImg.src = `.${
-            getPage()[1]
-          }/assets/img_item/CorsairK55RGBPRO.webp`;
-
-          li.append(itemImg, element.name, element.description, element.price);
-          allItems.append(li);
-        });
-      });
+    fetchItems(`traitement_filter.php?subCategory=` + categoryChild[i].id);
   });
 }
--- php/traitement_filter.php
+++ php/traitement_filter.php
@@ -4,10 +4,17 @@ require_once("./include/bdd.php");
 // $returnCategoryParent = $bdd->prepare('SELECT * FROM category WHERE id_parent = 0');
 // $returnCategoryParent->execute();
 // $resultCategoryParent = $returnCategoryParent->fetchAll(PDO::FETCH_OBJ);
-if (isset($_GET['subCategory'])){
-$request = $bdd->prepare("SELECT * FROM items INNER JOIN liaison_items_category WHERE liaison_items_category.id_category =  ? AND items.id = liaison_items_category.id_item");
-$request->execute([$_GET['subCategory']]);
-$result = $request->fetchAll(PDO::FETCH_ASSOC);
-$json = json_encode($result);
-echo $json;
+
+if (isset($_GET['subCategory'])) {
+    $request = $bdd->prepare("SELECT * FROM items INNER JOIN image ON items.id = image.id_item INNER JOIN liaison_items_category WHERE liaison_items_category.id_category =  ? AND items.id = liaison_items_category.id_item ");
+    $request->execute([$_GET['subCategory']]);
+    $result = $request->fetchAll(PDO::FETCH_ASSOC);
+    $json = json_encode($result);
+    echo $json;
+} else {
+    $requestAllItems = $bdd->prepare("SELECT * FROM items INNER JOIN image ON items.id = image.id_item ORDER BY items.price DESC");
+    $requestAllItems->execute();
+    $resultAllItems = $requestAllItems->fetchAll(PDO::FETCH_ASSOC);
+    $jsonAllItems = json_encode($resultAllItems);
+    echo $jsonAllItems;
 }
