b092ce2306b7268b13743fb0d84877fd0a0e6f38
Charles-Caltagirone <charles.dewidehem-caltagirone@laplateforme.io>
2023-06-06 12:01:24 +0200

    fonction checkstop créée et mise dans function.js

--- js/function.js
+++ js/function.js
@@ -82,3 +82,20 @@ function showPassword(button, password) {
     }
   });
 }
+
+// Fonction pour afficher le stock
+function checkStock(stock, message) {
+  if (stock == 0) {
+    message.innerText = "BIENTÔT DE RETOUR";
+    message.style.color = "red";
+    console.log("EPUISE");
+  } else if (stock <= 5) {
+    message.innerText = "STOCK LIMITE (restant : " + stock + ")";
+    message.style.color = "orange";
+    console.log("LIMITE");
+  } else {
+    message.innerText = "EN STOCK";
+    message.style.color = "green";
+    console.log("STOCK");
+  }
+}
