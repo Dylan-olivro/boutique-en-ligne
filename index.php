09728d1ac89741e3f0779290fe464b81bf49d697
dylan-olivro <dylan.olivro@laplateforme.io>
2023-06-17 15:13:35 +0200

    nouveau css index + ajout copyright + new BDD

--- index.php
+++ index.php
@@ -1,54 +1,126 @@
 <?php require_once('./php/include/required.php');
 
-$request = $bdd->prepare("SELECT *,count(*) FROM liaison_product_order INNER JOIN products ON liaison_product_order.product_id = products.product_id INNER JOIN images ON images.product_id = products.product_id WHERE image_main = 1 GROUP BY products.product_id ORDER BY count(*) DESC LIMIT 8");
+// Requête pour récupérer les produits les plus acheter
+$request = $bdd->prepare("SELECT *,count(*) FROM liaison_product_order INNER JOIN products ON liaison_product_order.product_id = products.product_id INNER JOIN images ON images.product_id = products.product_id WHERE image_main = 1 GROUP BY products.product_id ORDER BY count(*) DESC LIMIT 4");
 $request->execute();
 $result = $request->fetchAll(PDO::FETCH_OBJ);
-
-
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <?php require_once('./php/include/head.php') ?>
-    <title>Home</title>
+    <title>Index</title>
     <link rel="stylesheet" href="./css/index.css">
 </head>
 
 <body>
     <?php require_once('./php/include/header.php') ?>
     <main>
-        <section id="container">
+        <section id="Container">
 
-            <div class="populaire">
+            <!-- DIV POUR LES PRODUTIS LES PLUS POPULAIRES -->
+            <div class="MainContent">
                 <h2>LES PLUS POPULAIRES</h2>
-                <div class="productPop">
+                <div class="BoxProducts">
                     <?php
                     foreach ($result as $key) { ?>
-                        <div class="cardPop">
-                            <a href="./php/detail.php?id=<?= $key->image_id ?>" class="linkPop">
+                        <div class="CardProduct">
+                            <a href="./php/detail.php?id=<?= $key->image_id ?>" class="LinkProduct">
                                 <div class="divImg">
                                     <img src="./assets/img_item/<?= $key->image_name ?>" alt="">
                                 </div>
-                                <div class="divText">
-                                    <p class="productName"><?= CoupePhrase(htmlspecialchars($key->product_name), 40) ?></p>
-                                </div>
                             </a>
-                            <div class="priceAndCart">
-                                <p class="productPrice"><?= htmlspecialchars($key->product_price) ?>€</p>
-                                <form action="" method="post" id="formPop">
-                                    <button type="submit" name="addPop<?= $key->product_id ?>" id="addPop"><i class="fa-solid fa-cart-plus"></i></button>
-                                </form>
+
+                            <div class="BoxDetailProduct">
+
+                                <a href="./php/detail.php?id=<?= $key->image_id ?>" class="LinkProduct">
+                                    <div class="BoxProductName">
+                                        <p class="ProductName"><?= CoupePhrase(htmlspecialchars($key->product_name), 40) ?></p>
+                                    </div>
+                                </a>
+
+                                <div class="BoxPriceBtn">
+                                    <p class="ProductPrice"><?= htmlspecialchars($key->product_price) ?>€</p>
+                                    <?php if (isset($_SESSION['user'])) { ?>
+                                        <form action="" method="post" id="FormCart">
+                                            <button type="submit" name="ButtonAddCartPopular<?= $key->product_id ?>" id="ButtonAddCartPopular"><i class="fa-solid fa-cart-plus"></i></button>
+                                        </form>
+                                    <?php } ?>
+                                </div>
+
                             </div>
+
                         </div>
                     <?php
-                        if (isset($_POST['addPop' . $key->product_id])) {
+                        if (isset($_POST['ButtonAddCartPopular' . $key->product_id])) {
+                            // Récupère la quantité du produit
+                            $quantity = $bdd->prepare("SELECT cart_quantity FROM carts WHERE product_id = :product_id");
+                            $quantity->execute(['product_id' => $key->product_id]);
+                            $result_quantity = $quantity->fetch(PDO::FETCH_OBJ);
+
                             // Insert le produit de la page dans le panier en gérant la quantité
+                            if ($quantity->rowCount() > 0) {
+                                $updateQuantity = $bdd->prepare("UPDATE carts SET cart_quantity= :cart_quantity WHERE product_id = :product_id");
+                                $updateQuantity->execute([
+                                    'cart_quantity' => $result_quantity->cart_quantity + 1,
+                                    'product_id' => $key->product_id
+                                ]);
+                            } else {
+                                $insertQuantity = $bdd->prepare("INSERT INTO carts(user_id, product_id, cart_quantity) VALUES (:user_id,:product_id,:cart_quantity)");
+                                $insertQuantity->execute([
+                                    'user_id' => $_SESSION['user']->user_id,
+                                    'product_id' => $key->product_id,
+                                    'cart_quantity' => 1
+                                ]);
+                            }
+                        }
+                    }
+                    ?>
+                </div>
+            </div>
+
+            <!-- DIV POUR LES PRODUTIS LES PLUS ????????? -->
+            <div class="MainContent">
+                <h2>LES PLUS POPULAIRES</h2>
+                <div class="BoxProducts">
+                    <?php
+                    foreach ($result as $key) { ?>
+                        <div class="CardProduct">
+                            <a href="./php/detail.php?id=<?= $key->image_id ?>" class="LinkProduct">
+                                <div class="divImg">
+                                    <img src="./assets/img_item/<?= $key->image_name ?>" alt="">
+                                </div>
+                            </a>
+
+                            <div class="BoxDetailProduct">
+
+                                <a href="./php/detail.php?id=<?= $key->image_id ?>" class="LinkProduct">
+                                    <div class="BoxProductName">
+                                        <p class="ProductName"><?= CoupePhrase(htmlspecialchars($key->product_name), 40) ?></p>
+                                    </div>
+                                </a>
+
+                                <div class="BoxPriceBtn">
+                                    <p class="ProductPrice"><?= htmlspecialchars($key->product_price) ?>€</p>
+                                    <?php if (isset($_SESSION['user'])) { ?>
+                                        <form action="" method="post" id="FormCart">
+                                            <button type="submit" name="ButtonAddCartNew<?= $key->product_id ?>" id="ButtonAddCartNew"><i class="fa-solid fa-cart-plus"></i></button>
+                                        </form>
+                                    <?php } ?>
+                                </div>
+
+                            </div>
+
+                        </div>
+                    <?php
+                        if (isset($_POST['ButtonAddCartNew' . $key->product_id])) {
+                            // Récupère la quantité du produit
                             $quantity = $bdd->prepare("SELECT `cart_quantity` FROM `carts` WHERE product_id = :product_id");
                             $quantity->execute(['product_id' => $key->product_id]);
                             $result_quantity = $quantity->fetch(PDO::FETCH_OBJ);
 
+                            // Insert le produit de la page dans le panier en gérant la quantité
                             if ($quantity->rowCount() > 0) {
                                 $updateQuantity = $bdd->prepare("UPDATE `carts` SET `cart_quantity`= :cart_quantity WHERE product_id = :product_id");
                                 $updateQuantity->execute([
@@ -78,19 +150,14 @@ $result = $request->fetchAll(PDO::FETCH_OBJ);
 
 <?php
 // * Demander si required est néssecaire vu que ça empêche d'afficher les messages d'erreurs !
+// * TRAVAILLER LE CSS SUR DU 1920x1080
 
 // ? FAIRE UNE PAGE POUR VOIR TOUTES NOS COMMANDES
 // ? Quand les inputs sont différents de vide mettre leur border en --button-color
-// ? PASSER LE MESSAGE DE STOCK EPUISEE SUR LE DETAIL EN --> JAVASCRIPT
-// ? Faire un select sur les categories
 
 // ! VERIFIER TOUTES LES ERREURS POSSIBLE EN CHANGEANT LES GET
-// ! AJOUTER LES CONDITIONS POUR LES IMAGES DANS LE FORMULAIRE DU PRODUIT
 // ! INTVAL POUR LA CONNEXION
-// ! TRAVAILLER LE CSS SUR DU 1920/1080
-// ! Ajouter le css concernant le main sur toutes les pages
 // ! REMETTRE LE SETINTERVAL SUR LES FETCH DE L'ADMIN
-// ! Probleme si on ajoute 2 fois le même produit au panier et qu'on click sur supprimer, ça supprime les 2 (voir avec des data-id et uniqId() ou faire un systeme de quantité)
 
 // TODO: désactiver la touche ENTRER sur l'autocomplétion car elle envoie sur une page erreur
 // TODO: rajouter un header location après le post Delete de la page Cart
--- php/include/footer.php
+++ php/include/footer.php
@@ -1,5 +1,4 @@
 <?php
-
 if (getURL()[0][1] === '/index.php' || getURL()[0][1] === '/') {
     includeFooter('./');
 } else {
@@ -13,16 +12,17 @@ function includeFooter($image)
 { ?>
     <footer>
         <section class="footerCard">
-
+            <!-- Card Charles -->
             <div class="card-client">
                 <div class="user-picture">
-                <img src="<?= $image ?>assets/photoCharles.jpg" alt="">
+                    <img src="<?= $image ?>assets/photoCharles.jpg" alt="">
                 </div>
-                <p class="name-client"> Charles
 
-                </p>
+                <p class="name-client">Charles</p>
+
                 <div class="social-media">
-                <a href="https://charles-caltagirone.students-laplateforme.io/" target="_blank">
+
+                    <a href="https://charles-caltagirone.students-laplateforme.io/" target="_blank">
                         <svg fill="#ffffff" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="-2.9 -2.9 63.80 63.80" xml:space="preserve" stroke="#ffffff" stroke-width="4.466">
                             <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
                             <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#CCCCCC" stroke-width="0.11599999999999999"></g>
@@ -32,12 +32,14 @@ function includeFooter($image)
                         </svg>
                         <span class="tooltip-social">Portfolio</span>
                     </a>
+
                     <a href="https://github.com/charles-caltagirone" target="_blank">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                             <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
                         </svg>
                         <span class="tooltip-social">Github</span>
                     </a>
+
                     <a href="https://www.linkedin.com/in/charles-caltagirone/" target="_blank">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                             <path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"></path>
@@ -47,14 +49,14 @@ function includeFooter($image)
                 </div>
             </div>
 
+            <!-- Card Dylan -->
             <div class="card-client">
                 <div class="user-picture">
                     <img src="<?= $image ?>assets/Picsart_22-04-03_16-45-36-101.jpg" alt="">
-
                 </div>
-                <p class="name-client"> Dylan
 
-                </p>
+                <p class="name-client">Dylan</p>
+
                 <div class="social-media">
 
                     <a href="https://dylan-olivro.students-laplateforme.io/" target="_blank">
@@ -67,12 +69,14 @@ function includeFooter($image)
                         </svg>
                         <span class="tooltip-social">Portfolio</span>
                     </a>
+
                     <a href="https://github.com/dylan-olivro/boutique-en-ligne" target="_blank">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                             <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
                         </svg>
                         <span class="tooltip-social">Github</span>
                     </a>
+
                     <a href="https://www.linkedin.com/in/dylan-olivro/" target="_blank">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                             <path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"></path>
@@ -82,16 +86,18 @@ function includeFooter($image)
                 </div>
             </div>
 
+            <!-- Card Gaetan -->
             <div class="card-client">
                 <div class="user-picture">
                     <svg viewBox="0 0 448 512" xmlns="http://www.w3.org/2000/svg">
                         <path d="M224 256c70.7 0 128-57.31 128-128s-57.3-128-128-128C153.3 0 96 57.31 96 128S153.3 256 224 256zM274.7 304H173.3C77.61 304 0 381.6 0 477.3c0 19.14 15.52 34.67 34.66 34.67h378.7C432.5 512 448 496.5 448 477.3C448 381.6 370.4 304 274.7 304z"></path>
                     </svg>
                 </div>
-                <p class="name-client"> Gaetan
 
-                </p>
+                <p class="name-client">Gaetan</p>
+
                 <div class="social-media">
+
                     <a href="#">
                         <svg fill="#ffffff" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="-2.9 -2.9 63.80 63.80" xml:space="preserve" stroke="#ffffff" stroke-width="4.466">
                             <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
@@ -102,12 +108,14 @@ function includeFooter($image)
                         </svg>
                         <span class="tooltip-social">Portfolio</span>
                     </a>
+
                     <a href="#">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                             <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
                         </svg>
                         <span class="tooltip-social">Github</span>
                     </a>
+
                     <a href="#">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                             <path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"></path>
@@ -118,5 +126,6 @@ function includeFooter($image)
             </div>
 
         </section>
+        <p id="copyright">&copy; Copyright 2023 - Dylan Olivro - Charles Caltagirone- Gaetan Baudran Tous droits réservés</p>
     </footer>
 <?php }
